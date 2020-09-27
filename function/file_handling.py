5660cf8be536cf3d55a57864d171a28bdad97f35
chaya2z <itunano57@gmail.com>
2020-09-28 04:01:31 +0900

    Add: docstring to file_handling.py func
    Why: improve code readability

--- function/file_handling.py
+++ function/file_handling.py
@@ -1,14 +1,25 @@
 class FileHandling:
     @staticmethod
     def load_actual_brightness():
+        """
+        Loading '/sys/class/backlight/intel_backlight/actual_brightness'
+
+        :return: str value of the file
+        """
         actual_brightness_pass = '/sys/class/backlight/intel_backlight/actual_brightness'
         with open(actual_brightness_pass) as f:
-            s = f.read()
+            current_brightness = f.read()
         f.close()
-        return s
+        return current_brightness
 
     @staticmethod
     def write_brightness(brightness_value):
+        """
+        Update brightness to '/sys/class/backlight/intel_backlight/brightness'
+
+        :param brightness_value: new brightness value
+        :return: None
+        """
         update_value = brightness_value
         brightness_pass = '/sys/class/backlight/intel_backlight/brightness'
         with open(brightness_pass, mode='w') as f:
